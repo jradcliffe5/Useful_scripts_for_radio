83af283b31281c223649648d6831fc50459b9349
Jack Radcliffe <jack.radcliffe@postgrad.manchester.ac.uk>
2018-03-28 11:03:32 +0200

    more

--- adjust_model_CASA.py
+++ adjust_model_CASA.py
@@ -1,16 +1,10 @@
-from astropy.io import fits
 import numpy as np
 import sys, os
 
 def adjust_model(modelimage,thresh):
     print 'Removing negatives from %s' % modelimage
-    hdu = fits.open(modelimage)
-    imagedata = hdu['PRIMARY'].data
-    imagedata[imagedata<thresh] = 0
-    header = hdu['PRIMARY'].header
-    hdu.writeto('%s_modified.fits' % modelimage.split('.fits')[0], overwrite=True)
-    hdu.close()
-    print('DONE.\nOutput image: %s_modified.fits' % modelimage.split('.fits')[0])
+    immath(imagename=modelimage, model='evalexpr', expr='iif(IM0>=%.9f, IM0, 0.0)' % thresh, outfile='%s_modified.im' % modelimage)
+    print('DONE.\nOutput image: %s_modified.im' % modelimage)
 
 try:
     modelimage = str(sys.argv[sys.argv.index('adjust_model.py')+1])
@@ -24,3 +18,4 @@ try:
     adjust_model(modelimage,thresh)
 except IndexError:
     print 'Usage python adjust_model.py <modelfitsfile> <mode> <thresh>\nCurrent mode(s) are del_neg/thresh: delete negatives in model/threshold'
+s_no_neg.fits' % modelimage.split('.fits')[0])
